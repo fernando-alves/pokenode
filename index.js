bd5f21d1cc23ac9668803fe6bcff34cc8de8c01d
Fernando <fernandoasfj@gmail.com>
2017-12-01 13:28:54 -0300

    Add something special

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const pokedex = require('./lib/pokedex');
+const imageToAscii = require('image-to-ascii');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -13,8 +14,12 @@ app.get('/', (req, res) => {
 
 app.get('/pokedex/:pokemon', (req, res) => {
   const pokemon = req.params.pokemon;
-  console.log(`Pokedex para: ${pokemon}`);
-  res.send(pokedex.info(pokemon));
+  imageToAscii(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon}.png`, { colored: false, size: { height: '75%' }, white_bg: false }, (err, converted) => {
+    if (err) {
+      return res.sendStatus(404)
+    }
+    res.send(converted);
+  });
 });
 
 const server = app.listen(8080, () => {
